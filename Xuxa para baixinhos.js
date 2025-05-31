c54b7651cc140d27cde91c14e6db1f1653f41c3f
Apolo1Arthur <kirikokook@gmail.com>
2025-05-31 08:24:04 -0300

    Create Xuxa para baixinhos.js

---
+++ Xuxa para baixinhos.js
@@ -0,0 +1,10 @@
+const textos = [
+  "", // Slide 0 - Tela Inicial
+  "Capítulo 1: Nascida em 29 de janeiro de 1954, no Mississippi, Oprah Winfrey teve uma infância marcada por dificuldades extremas. Filha de uma mãe solteira adolescente, viveu em situação de pobreza extrema, vestindo roupas feitas de sacos de batata e enfrentando discriminação desde cedo.",
+  "Capítulo 2: Sua infância foi ainda mais desafiadora ao enfrentar abusos físicos e psicológicos. Aos nove anos, começou a sofrer abusos sexuais de parentes e conhecidos. Apesar disso, sempre buscou refúgio nos livros e na educação, desenvolvendo sua incrível habilidade de comunicação.",
+  "Capítulo 3: Aos 19 anos, Oprah tornou-se a primeira mulher negra a apresentar o noticiário em Nashville. Sua autenticidade, empatia e inteligência emocional logo a destacaram, levando-a a se tornar apresentadora de talk shows, onde revolucionaria a TV americana.",
+  "Capítulo 4: O 'The Oprah Winfrey Show' estreou em 1986 e rapidamente se tornou o programa de entrevistas mais assistido da história. Oprah abordava temas pessoais, sociais e espirituais, conectando-se com milhões de pessoas e quebrando tabus.",
+  "Capítulo 5: Fora das câmeras, Oprah tornou-se uma magnata da mídia, fundando sua própria produtora, a Harpo Productions. Investiu em educação, criou uma escola na África do Sul e foi a primeira mulher negra bilionária do mundo.",
+  "Capítulo 6: O legado de Oprah vai além da televisão. Ela inspirou gerações com sua história de superação, seu ativismo social e seu compromisso com a verdade. Oprah é símbolo de resiliência, compaixão e transformação global.",
+  "Menu de Capítulos: Clique em um dos botões abaixo para ir direto a um capítulo."
+];
